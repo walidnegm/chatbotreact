0c157ac7e84f9b41fe5775391227a180906651e8
Walid <walidnegm@gmail.com>
2024-10-28 16:00:48 -0400

    save

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Chatbot.css';
-import React, { useState, useEffect, useRef } from 'react';
-import './Chatbot.css';
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
