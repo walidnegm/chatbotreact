783bfd6cd25c1b2ff844694582dd8f33537b337c
Walid <walidnegm@gmail.com>
2024-11-02 10:19:50 -0400

    bothome

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Chatbot from './Chatbot';
+import BotHome from './BotHome';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chatbot Interface</h1>
-      <Chatbot />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Chatbot Interface</h1>
+        <Routes>
+          <Route path="/" element={<BotHome />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
